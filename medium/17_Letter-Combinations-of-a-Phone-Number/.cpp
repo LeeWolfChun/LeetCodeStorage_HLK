663820193a797826eaeabeb5908d0ca976022305
Her Luy Kwin <trung231007@gmail.com>
2026-09-19 10:29:25 +0700

    Implement letter combinations for phone number

---
+++ medium/17_Letter-Combinations-of-a-Phone-Number/.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector <string> letters = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    void scramble(vector <string> &result,string digits, int index,string path) {
+        int number = digits[index] - '0';
+        if (path.length() == digits.length()) {
+            result.push_back(path);
+        } else {
+            for ( auto c :letters[number]) {
+                scramble(result,digits,index + 1, path + c);
+            }
+        }
+    }
+    vector<string> letterCombinations(string digits) {   
+        vector <string> result;
+        string path = "";
+        scramble(result,digits,0, path);
+        return result;
+    }
+};
