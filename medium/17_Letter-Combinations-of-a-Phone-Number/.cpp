792c4cd0e17ee5959161ff6fb41984200084e908
Her Luy Kwin <trung231007@gmail.com>
2026-09-19 10:32:49 +0700

    Update .cpp

--- medium/17_Letter-Combinations-of-a-Phone-Number/.cpp
+++ medium/17_Letter-Combinations-of-a-Phone-Number/.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector <string> letters = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    void scramble(vector <string> &result,string digits, int index,string path) {
+    void scramble(vector <string>& result, string digits, int index, string path) {
         int number = digits[index] - '0';
         if (path.length() == digits.length()) {
             result.push_back(path);
         } else {
-            for ( auto c :letters[number]) {
-                scramble(result,digits,index + 1, path + c);
+            for (auto c : letters[number]) {
+                scramble(result, digits, index + 1, path + c);
             }
         }
     }
     vector<string> letterCombinations(string digits) {   
         vector <string> result;
-        string path = "";
-        scramble(result,digits,0, path);
+        scramble(result, digits, 0, "");
         return result;
     }
 };
